3deeb3e5c4a3d68a5745e82d5ba3e0a761088a0c
Yasteel Dhani <21824734@dut4life.ac.za>
2022-09-14 16:52:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button, Card, Form} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function Todo({ todo, index, markTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      
+    >
+      <p class="thin">
+      <span style={{ textDecoration: todo.isDone ? "line-through":"", textDecorationThickness:todo.isDone ? "2px":""}}>{todo.text}</span>
+      </p>
+      <div>
+        <Button variant="outline-success" onClick={() => markTodo(index)}>✓</Button>{' '}
+        <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
+      </div>
+    </div>
+  );
+}
+
+function FormTodo({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}> 
+    <Form.Group>
+      <Form.Label>Add a task to your list</Form.Label>
+      <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Enter a task" />
+    </Form.Group>
+    <br></br>
+    <Button variant="primary mb-3" type="submit">
+      Add
+    </Button>
+  </Form>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "Get some Starbucks :)",
+      isDone: false
+    }
+  ]);
+
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">To-do List</h1>
+        <FormTodo addTodo={addTodo} />
+        <div class="cards">
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                markTodo={markTodo}
+                removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
